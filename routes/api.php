ad48a9281a3b8756283054f795650d0030f7a027
KKazuki-MBP2018 <kazuki.k.bl8@gmail.com>
2019-10-07 20:54:28 +0900

    chapter-9 画像アップロードAPI実装

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
-
+use Illuminate\Http\Request;
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
-
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
-
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
-
 // ログインユーザー
 Route::get('/user', function () {
     return Auth::user();
 })->name('user');
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
